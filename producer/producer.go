7d9e70164dcab1de15ed2074f797f24bff87345d
Adam Thieme <adam@cs.ucla.edu>
2026-02-09 10:26:48 +0000

    more producer security

--- producer/producer.go
+++ producer/producer.go
@@ -7,28 +7,48 @@ import (
 	enc "github.com/named-data/ndnd/std/encoding"
 	"github.com/named-data/ndnd/std/engine"
 	"github.com/named-data/ndnd/std/log"
+	"github.com/named-data/ndnd/std/ndn"
 	"github.com/named-data/ndnd/std/object"
 	local_storage "github.com/named-data/ndnd/std/object/storage"
 	sec "github.com/named-data/ndnd/std/security"
 	"github.com/named-data/ndnd/std/security/keychain"
-	"github.com/named-data/ndnd/std/security/trust_schema"
+	"github.com/named-data/ndnd/std/security/signer"
 )
 
+// BasicSchema allows all data and suggests the first matching key in the keychain.
+type BasicSchema struct{}
+
+func (s *BasicSchema) Check(pkt enc.Name, cert enc.Name) bool {
+	return true // Trust everything (matching NullSchema behavior)
+}
+
+func (s *BasicSchema) Suggest(name enc.Name, kc ndn.KeyChain) ndn.Signer {
+	myname, _ := enc.NameFromStr("/ndn/repo.teame.dev/producer")
+	for _, id := range kc.Identities() {
+		if id.Name().IsPrefix(myname) {
+			if len(id.Keys()) > 0 {
+				return id.Keys()[0].Signer()
+			}
+		}
+	}
+
+	return signer.NewSha256Signer()
+}
 func main() {
 	log.Default().SetLevel(log.LevelTrace)
 	fmt.Println("starting")
 	engine := engine.NewBasicEngine(engine.NewDefaultFace())
 	engine.Start()
 	store := local_storage.NewMemoryStore()
-	client := object.NewClient(engine, store, nil)
 	target, _ := enc.NameFromStr("mytarget")
 	notify, _ := enc.NameFromStr("/ndn/drepo/notify")
+	prefix, _ := enc.NameFromStr("/ndn/repo.teame.dev/producer")
 
 	kc, err := keychain.NewKeyChain("dir:///home/adam/.ndn/keys", store)
 	if err != nil {
 		return
 	}
-	schema := trust_schema.NewNullSchema()
+	schema := &BasicSchema{}
 	testbedRootName, _ := enc.NameFromStr("/ndn/KEY/%27%C4%B2%2A%9F%7B%81%27/ndn/v=1651246789556")
 	trust, err := sec.NewTrustConfig(kc, schema, []enc.Name{testbedRootName})
 	if err != nil {
@@ -37,8 +57,13 @@ func main() {
 	trust.UseDataNameFwHint = true
 
 	// new client
-	client = object.NewClient(engine, store, trust)
-
+	client := object.NewClient(engine, store, trust)
+	client.SuggestSigner(prefix)
+	log.Debug(nil, "announce", "prefix", prefix)
+	client.AnnouncePrefix(ndn.Announcement{
+		Name:   prefix,
+		Expose: true,
+	})
 	command := tlv.Command{
 		Type:   "testtype",
 		Target: target,
